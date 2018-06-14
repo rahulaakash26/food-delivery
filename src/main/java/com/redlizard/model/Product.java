9f1b3b42edc772fe198e925deabfbe242b569b52
Rahul <rahulaakash26@gmail.com>
2018-06-14 15:18:47 +0530

    initial commit

---
+++ src/main/java/com/redlizard/model/Product.java
@@ -0,0 +1,9 @@
+package com.redlizard.model;
+
+public class Product {
+    private Integer id;
+    private String name;
+    private String description;
+    private Double price;
+    private boolean isAvailable;
+}
